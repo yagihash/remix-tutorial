80f0f03be3f8ac913a8579bd7ce305f0387559c3
Yu YAGIHASHI <yagihash@ssrf.dev>
2024-10-02 00:26:06 +0900

    adding style sheets with `links`

--- app/root.tsx
+++ app/root.tsx
@@ -5,6 +5,12 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import type { LinksFUnction } from "@remix-run/node";
+import appStylesHref from "./app.css?url";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: appStylesHref },
+];
 
 export default function App() {
   return (
