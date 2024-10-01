5a983dc860f2f5c25ff006e7efa6eccb8180d847
Yu YAGIHASHI <yagihash@ssrf.dev>
2024-10-02 00:54:32 +0900

    client side routing

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,6 @@
 import {
   Form,
+  Link,
   Links,
   Meta,
   Outlet,
@@ -43,10 +44,10 @@ export default function App() {
           <nav>
             <ul>
               <li>
-                <a href={`/contacts/1`}>Your Name</a>
+                <Link to={`/contacts/1`}>Your Name</Link>
               </li>
               <li>
-                <a href={`/contacts/2`}>Your Friend</a>
+                <Link to={`/contacts/2`}>Your Friend</Link>
               </li>
             </ul>
           </nav>
