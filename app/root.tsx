76537d16bd30c3b94ee5b7a8ce8a56b01094e43d
Yu YAGIHASHI <yagihash@ssrf.dev>
2024-10-02 23:09:45 +0900

    submitting form's onChange

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,7 @@ import {
   ScrollRestoration,
   useLoaderData,
   useNavigation,
+  useSubmit,
 } from "@remix-run/react";
 import type {
   LinksFunction,
@@ -39,6 +40,7 @@ export const loader = async ({
 export default function App() {
   const { contacts, q } = useLoaderData<typeof loader>();
   const navigation = useNavigation();
+  const submit = useSubmit();
   const [query, setQuery] = useState(q || "");
 
   useEffect(() => {
@@ -57,7 +59,13 @@ export default function App() {
         <div id="sidebar">
           <h1>Remix Contacts</h1>
           <div>
-            <Form id="search-form" role="search">
+            <Form
+              id="search-form"
+              onChange={(event) =>
+                submit(event.currentTarget)
+              }
+              role="search"
+            >
               <input
                 id="q"
                 aria-label="Search contacts"
