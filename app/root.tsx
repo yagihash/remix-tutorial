60afd4c699e6795187b8a71d11257c8badfae2df
Yu YAGIHASHI <yagihash@ssrf.dev>
2024-10-02 10:49:41 +0900

    active link styling

--- app/root.tsx
+++ app/root.tsx
@@ -1,9 +1,9 @@
 import { json, redirect } from "@remix-run/node";
 import {
   Form,
-  Link,
   Links,
   Meta,
+  NavLink,
   Outlet,
   Scripts,
   ScrollRestoration,
@@ -61,7 +61,16 @@ export default function App() {
               <ul>
                 {contacts.map((contact) => (
                   <li key={contact.id}>
-                    <Link to={`contacts/${contact.id}`}>
+                    <NavLink
+                      className={({ isActive, isPending }) =>
+                        isActive
+                          ? "active"
+                          : isPending
+                          ? "pending"
+                          : ""
+                        }
+                        to={`contacts/${contact.id}`}
+                      >
                       {contact.first || contact.last ? (
                         <>
                           {contact.first} {contact.last}
@@ -72,7 +81,7 @@ export default function App() {
                       {contact.favorite ? (
                         <span>★</span>
                       ) : null}
-                    </Link>
+                    </NavLink>
                   </li>
                 ))}
               </ul>
