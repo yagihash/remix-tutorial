8f8fcef5121540c9c1a6234343340ba7c75de543
Yu YAGIHASHI <yagihash@ssrf.dev>
2024-10-02 10:45:34 +0900

    redirecting new records to the edit page

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import { json } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
 import {
   Form,
   Link,
@@ -19,7 +19,7 @@ export const links: LinksFunction = () => [
 
 export const action = async () => {
   const contact = await createEmptyContact();
-  return json({ contact });
+  return redirect(`/contacts/${contact.id}/edit`);
 };
 
 export const loader = async() => {
